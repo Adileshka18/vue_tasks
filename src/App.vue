17ef8abe55c02c317f3f97f3c0a3fa42fc2ad5ae
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-03-03 23:37:09 +0300

    fff

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <h1>Добро пожаловать в Vue!</h1>
+    <p></p>
+  </header>
+</template>
+
+<style scoped>   
+header {
+  text-align: center;
+  margin-top: 50px;
+}
+
+h1 {
+  color: #42b983;
+  font-size: 2.5rem;
+}
+
+p {
+  font-size: 1.25rem;
+  color: #333;
+}
+</style>
