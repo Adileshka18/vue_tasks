59317b29ce45e74413a25c116dd654643f2e0bc0
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-03-15 01:12:22 +0300

    Task-10

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
-<template>
-  <div>
-    <button @click="showCurrentDate">Показать текущую дату</button>
-  </div>
-</template>
-
 <script>
 export default {
-  methods: {
-    showCurrentDate() {
-      const currentDate = new Date();
-      alert(`Текущая дата: ${currentDate.toLocaleString()}`);
+    methods: {
+        show() {
+            alert(new Date().toLocaleDateString());
+        }
     }
-  }
 }
 </script>
+
+<template>
+    <button @click="show">Показать дату</button>
+</template>
