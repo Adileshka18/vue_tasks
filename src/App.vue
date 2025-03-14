efcd3b6e6c713d99b07679c5c39688de491065df
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-03-15 00:20:59 +0300

    Task-8

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,7 @@ export default {
 
 data() {
 	return {
-		arr: ['x', 'y', 'z'],
-        arr1: [1, 2, 3],
+		obj: {x: 1, y: 2, z: 3},
 	}
 }
 
@@ -13,8 +12,6 @@ data() {
 
 
 <template>
-	<p>{{ arr[0] }}</p>
-	<p>{{ arr[1] }}</p>
-	<p>{{ arr[2] }}</p>
-    <p>{{ arr1[0] + arr1[1] + arr1[2]}}</p>
+	{{ obj.x + obj.y + obj.z}}
+	
 </template>
