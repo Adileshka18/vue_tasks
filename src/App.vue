14dd9582473b036e2548de5ea1f2a710778babb8
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-03-15 01:37:12 +0300

    Task-11

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <script>
 export default {
+    data() {
+        return {
+            num1: 1,
+            num2: 2,
+        };
+    },
     methods: {
         show() {
-            alert(new Date().toLocaleDateString());
+            alert(this.num1 + this.num2);
         }
     }
 }
 </script>
 
 <template>
-    <button @click="show">Показать дату</button>
+    <button @click="show">Показать сумму</button>
 </template>
