4261fc9e15f71410a591355853ff9917253ffea7
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-03-17 00:45:51 +0300

    Task-18

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,13 @@
-<script setup>
-import { ref } from 'vue';
-
-const visible = ref(true);
-
-// Функция для предотвращения перехода по ссылке
-const preventNavigation = (event) => {
-  event.preventDefault();
-};
-
-// Функция для обработки клика по кнопке (только первый клик)
-const clicked = ref(false);
-const handleClickOnce = () => {
-  if (!clicked.value) {
-    clicked.value = true;
-    console.log('Кнопка нажата один раз');
+<script>
+export default {
+  data() {
+    return {
+      hidden: false
+    };
   }
 };
 </script>
 
 <template>
-  <div>
-    <h1>Task#16</h1>
-    <a href="https://example.com" @click.prevent="preventNavigation">Запрещенная ссылка</a>
-    <br />
-    <button @click.once="handleClickOnce">Кликни один раз</button>
-    <br />
-    <h1>Task#17</h1>
-    <p v-if="visible">Этот абзац виден, если visible = true</p>
-    <p v-if="!visible">Этот абзац виден, если visible = false</p>
-  </div>
+  <p v-if="!hidden">text</p>
 </template>
