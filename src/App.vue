bac5127eca4d18f2b99607c6b93270b65756c240
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-03-20 00:06:54 +0300

    Task-23

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <script>
 export default {
   data() {
-	return {
-		isAdmin: true,
-	};
-},
+    return {
+      day: new Date().getDay(), // Получаем текущий день недели (0 - воскресенье, 6 - суббота)
+    };
+  },
 };
 </script>
 
 <template>
-	<p v-if="isAdmin">+++</p>
-	<p v-else>---</p>
+  <div>
+    <p v-if="day === 0">Sunday</p>
+    <p v-if="day === 1">Monday</p>
+    <p v-if="day === 2">Tuesday</p>
+    <p v-if="day === 3">Wednesday</p>
+    <p v-if="day === 4">Thursday</p>
+    <p v-if="day === 5">Friday</p>
+    <p v-if="day === 6">Saturday</p>
+  </div>
 </template>
