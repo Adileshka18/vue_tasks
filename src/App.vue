f35ad8d3be1556060ffd6cefd66617d849196632
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-03-19 23:24:17 +0300

    Task-22

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <script>
 export default {
   data() {
-    return {
-      visible: [false, false, false],
-    };
-  },
+	return {
+		isAdmin: true,
+	};
+},
 };
 </script>
 
 <template>
-  <div>
-    <button @click="visible[0] = !visible[0]">
-      {{ visible[0] ? 'Hide' : 'Show' }}
-    </button>
-    <p v-if="visible[0]">Text 1</p>
-
-    <button @click="visible[1] = !visible[1]">
-      {{ visible[1] ? 'Hide' : 'Show' }}
-    </button>
-    <p v-if="visible[1]">Text 2</p>
-
-    <button @click="visible[2] = !visible[2]">
-      {{ visible[2] ? 'Hide' : 'Show' }}
-    </button>
-    <p v-if="visible[2]">Text 3</p>
-  </div>
+	<p v-if="isAdmin">+++</p>
+	<p v-else>---</p>
 </template>
