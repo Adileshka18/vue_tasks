c85d9f875f1534835705d0097d15789b170fe579
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-03-19 22:55:53 +0300

    Task-21

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,19 @@ export default {
 
 <template>
   <div>
-    <button @click="visible[0] = !visible[0]">Toggle 1</button>
+    <button @click="visible[0] = !visible[0]">
+      {{ visible[0] ? 'Hide' : 'Show' }}
+    </button>
     <p v-if="visible[0]">Text 1</p>
 
-    <button @click="visible[1] = !visible[1]">Toggle 2</button>
+    <button @click="visible[1] = !visible[1]">
+      {{ visible[1] ? 'Hide' : 'Show' }}
+    </button>
     <p v-if="visible[1]">Text 2</p>
 
-    <button @click="visible[2] = !visible[2]">Toggle 3</button>
+    <button @click="visible[2] = !visible[2]">
+      {{ visible[2] ? 'Hide' : 'Show' }}
+    </button>
     <p v-if="visible[2]">Text 3</p>
   </div>
 </template>
