8388339457329b9dc680fd634da14695f6bd559b
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-03-20 00:50:28 +0300

    Task-30

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
 <template>
   <div>
     <!-- Задание 1 -->
-    <ul>
-      <li v-for="(value, key) in users" :key="key">{{ value }}</li>
-    </ul>
+    <div v-for="(item, index) in items" :key="index">
+      <p>{{ item }}</p>
+      <p class="divider"></p>
+    </div>
 
     <!-- Задание 2 -->
     <ul>
-      <li v-for="(value, key) in users" :key="key">{{ key }} - {{ value }}</li>
-    </ul>
-
-    <!-- Задание 3 -->
-    <ul>
-      <li v-for="(value, key, index) in users" :key="key">{{ key }} - {{ value }} - {{ index }}</li>
-    </ul>
-
-    <!-- Задание 4 -->
-    <ul>
-      <li v-for="(value, key, index) in users" :key="key">{{ key }} - {{ value }} - {{ index + 1 }}</li>
+      <li v-for="(item, index) in items" :key="index">
+        {{ item }}
+        <p v-if="index !== items.length - 1" class="divider"></p>
+      </li>
     </ul>
   </div>
 </template>
@@ -26,11 +20,7 @@
 export default {
   data() {
     return {
-      users: {
-        user1: '100$',
-        user2: '200$',
-        user3: '300$'
-      }
+      items: [1, 2, 3]
     };
   }
 }
