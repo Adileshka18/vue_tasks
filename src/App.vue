788c9e0fad576db321dc77c1ea29253262b37081
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-03-19 22:50:54 +0300

    Task-20

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,21 @@
 export default {
   data() {
     return {
-      visible: true,
+      visible: [false, false, false],
     };
   },
-
-  methods: {
-    show() {
-      this.visible = true;
-    },
-    hide() {
-      this.visible = false;
-    }
-  }
 };
 </script>
 
 <template>
-  <button v-if="!visible" @click="show">Показать</button>
-  <button v-if="visible" @click="hide">Скрыть</button>
-  <p v-if="visible">text</p>
+  <div>
+    <button @click="visible[0] = !visible[0]">Toggle 1</button>
+    <p v-if="visible[0]">Text 1</p>
+
+    <button @click="visible[1] = !visible[1]">Toggle 2</button>
+    <p v-if="visible[1]">Text 2</p>
+
+    <button @click="visible[2] = !visible[2]">Toggle 3</button>
+    <p v-if="visible[2]">Text 3</p>
+  </div>
 </template>
