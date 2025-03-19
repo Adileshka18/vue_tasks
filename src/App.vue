ce2a4f4e7cad17cbc8617b7f9ff1e04d13dd1d88
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-03-20 00:46:04 +0300

    Task-29

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
+<template>
+  <div>
+    <!-- Задание 1 -->
+    <ul>
+      <li v-for="(value, key) in users" :key="key">{{ value }}</li>
+    </ul>
+
+    <!-- Задание 2 -->
+    <ul>
+      <li v-for="(value, key) in users" :key="key">{{ key }} - {{ value }}</li>
+    </ul>
+
+    <!-- Задание 3 -->
+    <ul>
+      <li v-for="(value, key, index) in users" :key="key">{{ key }} - {{ value }} - {{ index }}</li>
+    </ul>
+
+    <!-- Задание 4 -->
+    <ul>
+      <li v-for="(value, key, index) in users" :key="key">{{ key }} - {{ value }} - {{ index + 1 }}</li>
+    </ul>
+  </div>
+</template>
+
 <script>
 export default {
   data() {
     return {
-      arr: ['x', 'y', 'z'],
+      users: {
+        user1: '100$',
+        user2: '200$',
+        user3: '300$'
+      }
     };
-  },
-};
+  }
+}
 </script>
-
-<template>
-  <ul>
-    <li v-for="(elem, key) in arr" :key="key">{{ key }}</li>
-  </ul>
-</template>
