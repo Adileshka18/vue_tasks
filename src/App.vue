cb15ef56a4b497e59fbcf82c1ab505341c2cb5e0
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-04-09 16:55:29 +0300

    Task-39

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <p :class="obj">Этот абзац будет иметь классы в зависимости от значений в объекте.</p>
+    <!-- Задание 1: Применение объекта с классами для скрытия элемента -->
+    <p :class="obj">Этот абзац будет скрыт, если obj.hidden == true</p>
+
+    <!-- Задание 2: Кнопка для показа элемента -->
+    <button @click="showElement">Показать элемент</button>
+
+    <!-- Задание 3: Кнопка для скрытия элемента -->
+    <button @click="hideElement">Скрыть элемент</button>
+
+    <!-- Задание 4: Кнопка для тогглирования видимости элемента -->
+    <button @click="toggleElement">Тогглить элемент</button>
   </div>
 </template>
 
@@ -9,20 +19,26 @@ export default {
   data() {
     return {
       obj: {
-        done: true,
-        selected: false,
+        hidden: true,  
       },
     };
+  },
+  methods: {
+    showElement() {
+      this.obj.hidden = false;  
+    },
+    hideElement() {
+      this.obj.hidden = true;   
+    },
+    toggleElement() {
+      this.obj.hidden = !this.obj.hidden;  
+    },
   },
 };
 </script>
 
 <style scoped>
-.done {
-  color: green;
-}
-
-.selected {
-  background-color: yellow;
+.hidden {
+  display: none; 
 }
 </style>
