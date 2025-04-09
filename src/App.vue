4f3786200c53df3c0929b89acd5c3b0b793df6fb
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-04-09 17:14:09 +0300

    Task-47

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,35 @@
+<template>
+  <div>
+    <!-- Задание 1 -->
+    <label>
+      <input type="checkbox" value="English" v-model="languages"> English
+    </label>
+    <label>
+      <input type="checkbox" value="Spanish" v-model="languages"> Spanish
+    </label>
+    <label>
+      <input type="checkbox" value="French" v-model="languages"> French
+    </label>
+    <label>
+      <input type="checkbox" value="German" v-model="languages"> German
+    </label>
+    <label>
+      <input type="checkbox" value="Russian" v-model="languages"> Russian
+    </label>
+
+    <!-- Задание 2 -->
+    <ul>
+      <li v-for="(language, index) in languages" :key="index">{{ language }}</li>
+    </ul>
+  </div>
+</template>
+
 <script>
 export default {
   data() {
     return {
-      isChecked: false,
+      languages: [],
     };
   },
 };
 </script>
-
-<template>
-  <div>
-    <input type="checkbox" v-model="isChecked" />
-    <p v-if="isChecked">Абзац отображается, потому что checkbox отмечен.</p>
-  </div>
-</template>
