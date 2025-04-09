132d9e24012efe9f2325a770728c26d3c957f4b5
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-04-09 23:23:16 +0300

    Task-58

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<!-- Employee.vue -->
+<template>
+  <tr>
+    <td>{{ employee.name }}</td>
+    <td>{{ employee.salary }}</td>
+    <td>{{ employee.age }}</td>
+    <td>
+      <a href="#" @click="removeEmployee">Delete</a>
+    </td>
+    <td>
+      <a href="#" @click="editEmployee">Edit</a>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  props: {
+    employee: Object,
+    index: Number,
+  },
+  methods: {
+    removeEmployee() {
+      this.$emit('remove', this.index);
+    },
+    editEmployee() {
+      this.$emit('edit', this.index);
+    },
+  },
+};
+</script>
