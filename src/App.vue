e35042ab25cfe5333c78e3aef8d9dc35340371f4
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-04-09 17:11:34 +0300

    Task-46

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,16 @@
-<template>
-  <div>
-    <!-- Задание 1 -->
-    <textarea v-model="text" placeholder="Введите текст"></textarea>
-    <p>{{ text }}</p>
-
-    <!-- Задание 2 -->
-    <textarea v-model="textArea" placeholder="Введите текст"></textarea>
-    <button @click="displayWords">Показать слова</button>
-    <ul>
-      <li v-for="(word, index) in words" :key="index">{{ word }}</li>
-    </ul>
-  </div>
-</template>
-
 <script>
 export default {
   data() {
     return {
-      text: '',
-      textArea: '',
-      words: [],
+      isChecked: false,
     };
-  },
-  methods: {
-    displayWords() {
-      this.words = this.textArea.split(' ');
-    },
   },
 };
 </script>
+
+<template>
+  <div>
+    <input type="checkbox" v-model="isChecked" />
+    <p v-if="isChecked">Абзац отображается, потому что checkbox отмечен.</p>
+  </div>
+</template>
