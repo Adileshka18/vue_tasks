b1d12e7a9f9a5cf15697debc07e9eed7ab91d263
Adile Ashirova <ashirova.a.i.23@gmail.com>
2025-04-09 17:07:27 +0300

    Task-44

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <div>
     <!-- Задание 1 -->
-    <input v-model="text" type="text" placeholder="Введите текст">
-    <p>{{ text }}</p>
+    <input v-model.number="number" type="number" placeholder="Введите число">
+    <button @click="calculateSquareRoot">Показать квадратный корень</button>
+    <p>{{ squareRoot }}</p>
 
     <!-- Задание 2 -->
-    <input v-model="text" type="text" placeholder="Введите текст">
-    <p>{{ text.toUpperCase() }}</p>
+    <input v-model.number="number1" type="number" placeholder="Введите первое число">
+    <input v-model.number="number2" type="number" placeholder="Введите второе число">
+    <button @click="calculateSum">Показать сумму</button>
+    <p>{{ sum }}</p>
 
     <!-- Задание 3 -->
-    <input v-model.number="number" type="number" placeholder="Введите число">
-    <p>{{ number * number }}</p>
+    <input v-model="text1" type="text" placeholder="Введите текст 1">
+    <input v-model="text2" type="text" placeholder="Введите текст 2">
+    <button @click="swapText">Поменять тексты</button>
+    <p>{{ text1 }}</p>
+    <p>{{ text2 }}</p>
+
+    <!-- Задание 4 -->
+    <input v-model="fullName" type="text" placeholder="Введите ФИО">
+    <button @click="splitName">Разделить ФИО</button>
+    <p>{{ surname }}</p>
+    <p>{{ name }}</p>
+    <p>{{ patronymic }}</p>
   </div>
 </template>
 
@@ -18,9 +31,37 @@
 export default {
   data() {
     return {
-      text: '',
       number: 0,
+      squareRoot: null,
+      number1: 0,
+      number2: 0,
+      sum: 0,
+      text1: '',
+      text2: '',
+      fullName: '',
+      surname: '',
+      name: '',
+      patronymic: '',
     };
+  },
+  methods: {
+    calculateSquareRoot() {
+      this.squareRoot = Math.sqrt(this.number);
+    },
+    calculateSum() {
+      this.sum = this.number1 + this.number2;
+    },
+    swapText() {
+      const temp = this.text1;
+      this.text1 = this.text2;
+      this.text2 = temp;
+    },
+    splitName() {
+      const parts = this.fullName.split(' ');
+      this.surname = parts[0] || '';
+      this.name = parts[1] || '';
+      this.patronymic = parts[2] || '';
+    },
   },
 };
 </script>
